e7f33d3a317151fa186fce4fe497a3fd07430ea7
Sergey Iryupin <sergey.iryupin@lightcomp.cz>
2022-06-08 12:44:19 +0200

    second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,27 @@
-public class HelloWorld {
+class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, javaschoolboy!");
+        Cat cat = new Cat("Barsik", "white", 2);
+        System.out.println(cat.voice());
+        System.out.println(cat.name + ", " + cat.color + ", " + cat.age);
+    }
+}
+
+class Cat {
+    String name;
+    String color;
+    int age;
+
+    Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    Cat() {
+        System.out.println("Constructor");
+    }
+
+    String voice() {
+        return "meow!";
     }
 }
